f8c10ca07490c6806de36f859308574508da9bc0
Utkarsh Arora <arorautkarsh2002@gmail.com>
2022-07-16 21:46:36 +0530

    Added Profile Pic as well as empty Leaderboard fragment

--- app/src/main/java/com/bitwisor/quizy/fragments/HomeFragment.kt
+++ app/src/main/java/com/bitwisor/quizy/fragments/HomeFragment.kt
@@ -2,23 +2,29 @@ package com.bitwisor.quizy.fragments
 
 import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.bitwisor.quizy.JoinActivity
 import com.bitwisor.quizy.LoginActivity
 import com.bitwisor.quizy.R
 import com.bitwisor.quizy.databinding.FragmentHomeBinding
+import com.bitwisor.quizy.utils.User
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.ktx.Firebase
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.squareup.picasso.Picasso
 
 
 class HomeFragment : Fragment() {
     lateinit var binding:FragmentHomeBinding
     lateinit var firebaseAuth: FirebaseAuth
+    lateinit var database:FirebaseDatabase
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -30,7 +36,23 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         firebaseAuth = FirebaseAuth.getInstance()
+        database = FirebaseDatabase.getInstance()
         binding.apply {
+
+
+
+            LeaderBoardView.setOnClickListener {
+                if(firebaseAuth.currentUser != null)
+                {
+                    findNavController().navigate(R.id.action_homeFragment_to_totalQuizzesInfoFragment)
+                }
+                else
+                {
+                    Toast.makeText(requireContext(), "Please Login First to View stats of your created quizzes", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+
             createQuizBtn.setOnClickListener {
                 if (firebaseAuth.currentUser != null){
                     findNavController().navigate(R.id.action_homeFragment_to_createFragment)
@@ -41,6 +63,35 @@ class HomeFragment : Fragment() {
                     requireActivity().finish()
                 }
             }
+
+
+            if(firebaseAuth.currentUser!=null)
+            {
+                database.reference.child("UserProfiles").child(FirebaseAuth.getInstance().uid!!)
+                    .addListenerForSingleValueEvent(object : ValueEventListener {
+                        override fun onDataChange(snapshot: DataSnapshot) {
+                            val users: User? = snapshot.getValue(User::class.java)
+                            if (users != null) {
+                                Picasso.get()
+                                    .load(users.profilepic)
+                                    .placeholder(R.drawable.scientist).into(binding.profileImage)
+                            }
+                        }
+
+                        override fun onCancelled(error: DatabaseError) {}
+                    })
+
+
+            }
+
+
+
+
+
+
+
+
+
             joinQuizBtn.setOnClickListener {
                 val i = Intent(requireActivity(),JoinActivity::class.java)
                 startActivity(i)
