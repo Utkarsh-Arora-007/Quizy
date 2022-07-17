238f0ec93707b3770dd366ff3451f0c4322be6a6
shivenducs1136 <77795429+shivenducs1136@users.noreply.github.com>
2022-07-17 13:36:58 +0530

    ALL ERROR SOLVED

--- app/src/main/java/com/bitwisor/quizy/fragments/AddQuestionsFragment.kt
+++ app/src/main/java/com/bitwisor/quizy/fragments/AddQuestionsFragment.kt
@@ -1,13 +1,18 @@
 package com.bitwisor.quizy.fragments
 
+import android.app.Dialog
 import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.activity.OnBackPressedCallback
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import androidx.navigation.fragment.findNavController
 import com.bitwisor.quizy.LoginActivity
+import com.bitwisor.quizy.MainActivity
 import com.bitwisor.quizy.R
 import com.bitwisor.quizy.databinding.FragmentAddQuestionsBinding
 import com.bitwisor.quizy.utils.QuizQuestions
@@ -36,6 +41,7 @@ class AddQuestionsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentAddQuestionsBinding.inflate(layoutInflater)
+
         return binding.root
     }
 
@@ -43,6 +49,7 @@ class AddQuestionsFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         binding.mprogress.visibility = View.GONE
         binding.donelottie.visibility = View.GONE
+
         var bundle = this.arguments
         if(bundle!=null){
             totalQuestions = bundle.getInt("NoOfQuestion")
@@ -65,17 +72,18 @@ class AddQuestionsFragment : Fragment() {
         }
         binding.addnextQuestionbtn.setOnClickListener {
             val qnu = questionNo
+
             question = binding.questionedittxt.text.toString()
             option1 = binding.optionAedittxt.text.toString()
             option2 = binding.optionBedittxt.text.toString()
             option3 = binding.optionCedittxt.text.toString()
             option4 = binding.optionDedittxt.text.toString()
             correctOption = binding.correctOptiontxt.text.toString()
-            if(!question.isNullOrEmpty() and
-                !option1.isNullOrEmpty() and
-                !option2.isNullOrEmpty() and
-                !option3.isNullOrEmpty() and
-                !option4.isNullOrEmpty() and
+            if(!question.isNullOrEmpty() &&
+                !option1.isNullOrEmpty() &&
+                !option2.isNullOrEmpty() &&
+                !option3.isNullOrEmpty() &&
+                !option4.isNullOrEmpty() &&
                 !correctOption.isNullOrEmpty()){
                 var options = arrayOf(option1,option2,option3,option4)
                 if(isCorrectOptionValid(options,correctOption)){
@@ -145,6 +153,7 @@ class AddQuestionsFragment : Fragment() {
         }
         binding.doneQuestionAddingbtn.setOnClickListener {
             findNavController().popBackStack()
+
         }
     }
 
