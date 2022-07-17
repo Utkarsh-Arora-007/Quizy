abcef757cf3f517fd024a9352d9fc1582f693ef1
Utkarsh Arora <arorautkarsh2002@gmail.com>
2022-07-17 16:56:18 +0530

    Final Call

--- app/src/main/java/com/bitwisor/quizy/fragments/DetailsPerQuizFragment.kt
+++ app/src/main/java/com/bitwisor/quizy/fragments/DetailsPerQuizFragment.kt
@@ -1,10 +1,16 @@
 package com.bitwisor.quizy.fragments
 
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.os.Environment
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.core.content.PermissionChecker.checkSelfPermission
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.bitwisor.quizy.Adapters.DetailsPerQuizAdapter
@@ -20,13 +26,21 @@ import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
+import com.itextpdf.text.pdf.PdfWriter
+import org.w3c.dom.Document
+import java.io.FileOutputStream
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.collections.ArrayList
 
 class DetailsPerQuizFragment : Fragment() {
     lateinit var binding: FragmentDetailsPerQuizBinding
     private lateinit var adapter: DetailsPerQuizAdapter
     private lateinit var detailsArrayList: ArrayList<DetailsPerQuiz>
     private lateinit var userRecyclerView: RecyclerView
+    private val STORAGE_CODE = 1001
     var quizId=""
+    lateinit var export_pdf:ImageView
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -40,6 +54,25 @@ class DetailsPerQuizFragment : Fragment() {
         userRecyclerView=binding.mrecview
         binding.leaderProgresscircle.visibility = View.VISIBLE
         detailsArrayList= arrayListOf<DetailsPerQuiz>()
+        export_pdf = binding.exportPdf
+
+//        export_pdf.setOnClickListener {
+//            if(Build.VERSION.SDK_INT > Build.VERSION_CODES.M)
+//            {
+//                if(context!!.checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)== PackageManager.PERMISSION_DENIED){
+//                    val permission= arrayOf(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+//                    requestPermissions(permission,STORAGE_CODE)
+//
+//                }else{
+//                    savePDF()
+//                }
+//            }
+//            else
+//            {
+//                savePDF()
+//            }
+//
+//        }
         var bundle = arguments
         if (bundle!=null){
             quizId = bundle.getString("QuizId","0000").toString()
@@ -69,12 +102,37 @@ class DetailsPerQuizFragment : Fragment() {
 
                 }
 
+
                 override fun onCancelled(error: DatabaseError) {
                     Snackbar.make(view,"Some Error Occured", Snackbar.LENGTH_SHORT).show()
                 }
 
             })
     }
+
+//    private fun savePDF() {
+//        val mDoc= com.itextpdf.text.Document()
+//        val mFileName = SimpleDateFormat("yyMMdd_HHmmss", Locale.getDefault())
+//            .format(System.currentTimeMillis())
+//
+//        val mFilePath = Environment.getExternalStorageDirectory().toString() + "/" + mFileName + ".pdf"
+//
+//
+//        try {
+//
+//            PdfWriter.getInstance(mDoc, FileOutputStream(mFilePath))
+//            mDoc.open()
+//
+//            val data=
+//
+//        }catch (e:Exception){
+//
+//            Toast.makeText(requireContext(), ""+e.toString(), Toast.LENGTH_SHORT).show()
+//
+//
+//        }
+//
+//    }
 
 
 }
